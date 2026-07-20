3cce1e6e9cda52891c77310a333c82f4bd8ab98c
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-20 19:45:57 +0200

    Update components/AIAdvisorWrapper.tsx

--- components/AIAdvisorWrapper.tsx
+++ components/AIAdvisorWrapper.tsx
@@ -1,9 +1,9 @@
-
 'use client';
 import { useApp } from '../context/AppContext';
 import AIAdvisor from './AIAdvisor';
 
 export default function AIAdvisorWrapper() {
-  const { language } = useApp();
+  const { language, currentPath } = useApp();
+  if (currentPath === '/dashboard') return null;
   return <AIAdvisor language={language} />;
 }
